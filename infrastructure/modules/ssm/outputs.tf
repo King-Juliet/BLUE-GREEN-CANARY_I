5895de22ee5ca83c973753d5ca0deae9fb64814c
King-Juliet <chibuokejuliet@gmail.com>
2026-09-17 17:23:59 +0100

    updated the outputs.tf file of the ssm module to fix reference mismatch

--- infrastructure/modules/ssm/outputs.tf
+++ infrastructure/modules/ssm/outputs.tf
@@ -1,7 +1,15 @@
+# output "parameter_arn" {
+#   value = aws_ssm_parameter.db_password.arn
+# }
+
+# output "parameter_name" {
+#   value = aws_ssm_parameter.db_password.name
+# }
+
 output "parameter_arn" {
-  value = aws_ssm_parameter.db_password.arn
+  value = aws_ssm_parameter.ssm_parameter.arn
 }
 
 output "parameter_name" {
-  value = aws_ssm_parameter.db_password.name
+  value = aws_ssm_parameter.ssm_parameter.name
 }
